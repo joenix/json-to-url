8f9c0b0b9075b20812ad83a88010fe0f6562855f
Joenix <joenix@qq.com>
2021-10-31 04:56:30 +0800

    feat: version

--- index.js
+++ index.js
@@ -4,37 +4,34 @@
  * Parse Json To Url Query
  * ===== ===== =====
  */
-module.exports = function (data, mode)
-{
+module.exports = function(data, mode) {
+  if (!(data instanceof Object)) {
+    return data;
+  }
 
-	if ( !(data instanceof Object) ) {
-		return data;
-	}
+  data = JSON.stringify(data);
 
-	data = JSON.stringify( data );
+  if (mode == true) {
+    return data;
+  }
 
-	if (mode == true) {
-		return data;
-	}
+  return (function(exp) {
+    data = data.replace(exp.wild, function(word, $1, $2, $3, $4, $5) {
+      return [$2, "=", $5].join("");
+    });
 
-	return (function (exp) {
-
-		data = data.replace(exp.wild, function (word, $1, $2, $3, $4, $5) {
-			return [$2, '=', $5].join('');
-		});
-
-		return data.replace(exp.trim, '').replace(exp.place, '&');
-	})
-	(
-		// Exp
-		{
-			// Wild
-			wild: new RegExp(/(")?([\w\-\!\@\#\$\%\^\&\*\+\(\)\{\}\[\]\:\;\,\.]+)(")?:(")?([\w\-\!\@\#\$\%\^\&\*\+\(\)\{\}\[\]\:\;\,\.]+)(")?/g),
-			// Trim
-			trim: new RegExp(/^{|}$/g),
-			// Place
-			place: new RegExp(/\,/g)
-		}
-	);
-
-}
+    return data.replace(exp.trim, "").replace(exp.place, "&");
+  })(
+    // Exp
+    {
+      // Wild
+      wild: new RegExp(
+        /(")?([\w\-\!\@\#\$\%\^\&\*\+\(\)\{\}\[\]\:\;\,\.]+)(")?:(")?([\w\-\!\@\#\$\%\^\&\*\+\(\)\{\}\[\]\:\;\,\.]+)(")?/g
+      ),
+      // Trim
+      trim: new RegExp(/^{|}$/g),
+      // Place
+      place: new RegExp(/\,/g)
+    }
+  );
+};
